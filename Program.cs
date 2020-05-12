ed2808dc17595d677c6592187f954c20fcd82766
adriano.exe <58786675+adriano@users.noreply.github.com>
2020-05-12 19:37:55 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vitima
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string telefone;
+
+            Console.WriteLine("Responda com S ou N:");
+            Console.Write("Telefonou para vitima? ");
+            Console.Write("Esteve no local do crime? ");
+            Console.Write("Mora perto da vitima? ");
+            Console.Write("Devia para a vitima? ");
+            Console.Write("Ja trabalhou com a vitima? ");
+            telefone = Console.ReadLine();
+        }
+    }
+}
